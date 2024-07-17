08a194839c62e108fc68fde2b66b86d79773fdad
Nick Gregory <computerfreak97@gmail.com>
2024-07-17 19:29:42 -0400

    conditional tests

--- src/bismuth/ml/connectors/test_connectors.py
+++ src/bismuth/ml/connectors/test_connectors.py
@@ -1,3 +1,4 @@
+import psycopg
 from .file import FileConnector
 from .postgresql import PostgresConnector
 from .snowflake import SnowflakeConnector
@@ -6,9 +7,13 @@ import os
 import pytest
 
 def setup_postgres():
+    try:
+        pg = PostgresConnector("postgresql://postgres:password@localhost:5432/postgres", "iris")
+    except psycopg.OperationalError:
+        return pytest.param(None, marks=pytest.mark.skip(reason="no postgres server running"))
+
     dataset = csv.reader(open("src/bismuth/ml/connectors/testdata/iris.csv"))
     next(dataset) # skip header
-    pg = PostgresConnector("postgresql://postgres:password@localhost:5432/postgres", "iris")
     with pg.conn.cursor() as cur:
         cur.execute("DROP TABLE IF EXISTS iris")
         cur.execute("CREATE TABLE iris (sepal_length FLOAT, sepal_width FLOAT, petal_length FLOAT, petal_width FLOAT, variety TEXT)")
@@ -16,10 +21,11 @@ def setup_postgres():
         pg.conn.commit()
     return pg
 
-@pytest.fixture(params=[
-    FileConnector("src/bismuth/ml/connectors/testdata/iris.csv"),
-    setup_postgres(),
-    SnowflakeConnector({
+def setup_snowflake():
+    if "SNOWFLAKE_USER" not in os.environ:
+        return pytest.param(None, marks=pytest.mark.skip(reason="snowflake credentials not present"))
+
+    return SnowflakeConnector({
         "user": os.environ["SNOWFLAKE_USER"],
         "password": os.environ["SNOWFLAKE_PASSWORD"],
         "account": os.environ["SNOWFLAKE_ACCOUNT"],
@@ -27,6 +33,11 @@ def setup_postgres():
         "database": "TEST",
         "schema": "PUBLIC",
     }, "IRIS")
+
+@pytest.fixture(params=[
+    FileConnector("src/bismuth/ml/connectors/testdata/iris.csv"),
+    setup_postgres(),
+    setup_snowflake(),
 ])
 def connector(request):
     return request.param
@@ -39,7 +50,7 @@ def test_count(connector):
 
 def test_cardinality(connector):
     assert connector.approx_cardinality("variety") == 3
-    
+
 def test_sample(connector):
     sample1 = connector.sample(3, seed=42)
     sample2 = connector.sample(3, seed=42)
