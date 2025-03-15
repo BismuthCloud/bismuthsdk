4aab0691cafcb31293d3726799b08c0734797ed0
Nick Gregory <computerfreak97@gmail.com>
2025-03-15 11:34:48 -0400

    example updates

--- python/examples/scan.py
+++ python/examples/scan.py
@@ -2,6 +2,7 @@ import logging
 import os
 import pathlib
 import sys
+from git import Repo
 from bismuthsdk import BismuthClient
 
 if __name__ == "__main__":
@@ -10,10 +11,15 @@ if __name__ == "__main__":
     logging.getLogger("git").setLevel(logging.DEBUG)
 
     repo_dir = pathlib.Path(sys.argv[1])
+    repo = Repo(repo_dir)
 
     api = BismuthClient(api_key=os.environ["BISMUTH_API_KEY"])
+    # try:
+    #     api.get_project(repo_dir.name).delete()
+    # except:
+    #     pass
     project = api.load_project(repo_dir)
-    branch = project.get_branch("main")
+    branch = project.get_branch(repo.active_branch.name)
     res = branch.scan(max_subsystems=1)
 
     print("Scanned subsystems:", ",".join(sub.name for sub in res.scanned_subsystems))
--- python/examples/search.py
+++ python/examples/search.py
@@ -9,6 +9,11 @@ if __name__ == "__main__":
     logging.getLogger("git").setLevel(logging.DEBUG)
 
     api = BismuthClient(api_key=os.environ["BISMUTH_API_KEY"])
-    project = api.load_project(pathlib.Path(__file__).parent.parent.parent)
+    repo = pathlib.Path(__file__).parent.parent.parent
+    try:
+        api.get_project(repo.name).delete()
+    except:
+        pass
+    project = api.load_project(repo)
     results = project.get_branch("main").search("query", top=3)
     print(results)
